7460a0a0ff4a7a7e5fdd6d3c0249b71ddaae63ac
AndrewSeres <93342731+AndrewSeres@users.noreply.github.com>
2026-02-19 09:54:59 +0100

    add: service for reading excel files (streaming) + update pom, formatting

--- src/main/java/com/andrej/App.java
+++ src/main/java/com/andrej/App.java
@@ -1,13 +1,46 @@
 package com.andrej;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.andrej.service.ExcelReaderService;
+import com.andrej.validators.PrimeNumberValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.List;
+
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+        File file = new File(filePath);
+
+        if (!file.exists() || !file.isFile()) {
+            logger.error("The file '{}' does not exist or is not a valid file.", filePath);
+            System.exit(1);
+        }
+
+
+        ExcelReaderService reader = new ExcelReaderService();
+        PrimeNumberValidator validator = new PrimeNumberValidator();
+
+        try {
+            List<Long> numbers = reader.readNumbers(file);
+
+            for (Long n : numbers) {
+                if (validator.isValidPrime(n)) {
+                    logger.info("{}", n);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred during procesing: ", e);
+            System.exit(1);
+        }
     }
 }
